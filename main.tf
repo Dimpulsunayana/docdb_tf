42de492d1648ee36877f94145b9354a90d2c7068
windows 10 <dimpulsunayana205@gmail.com>
2023-04-01 11:15:05 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -37,9 +37,13 @@ resource "aws_db_subnet_group" "default" {
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "${var.env}-db-cluster"
   engine                  = "docdb"
+  engine_version          = var.engine_version
   master_username         = "dimpul"
   master_password         = "dimpul123"
   storage_encrypted       = true
+  skip_final_snapshot     = true
+  db_subnet_group_name   = aws_docdb_subnet_group.default.name
+  vpc_security_group_ids = aws_security_group.allow_tls.id
 
   tags       = merge(
     local.common_tags,
@@ -51,5 +55,5 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = 1
   identifier         = "docdb-cluster-demo-${count.index}"
   cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = "db.t3.medium"
+  instance_class     = var.instance_class
 }
--- var.tf
+++ var.tf
@@ -2,3 +2,5 @@ variable "main_vpc" {}
 variable "allow_cidr" {}
 variable "env" {}
 variable "subnet_ids" {}
+variable "engine_version" {}
+variable "instance_class" {}
