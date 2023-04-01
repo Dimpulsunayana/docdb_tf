49f8ab2f149a87210f1b2afc311da761b9b8b59e
windows 10 <dimpulsunayana205@gmail.com>
2023-04-01 11:29:21 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_docdb_cluster" "docdb" {
   storage_encrypted       = true
   skip_final_snapshot     = true
   db_subnet_group_name   = aws_docdb_subnet_group.default.name
-  vpc_security_group_ids = aws_security_group.allow_tls.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags       = merge(
     local.common_tags,
