b3b157183a99cc4644df8b1d56c9a0b4cae8504e
windows 10 <dimpulsunayana205@gmail.com>
2023-04-01 11:02:14 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -46,3 +46,10 @@ resource "aws_docdb_cluster" "docdb" {
     { name = "${var.env}-db_cluster" }
   )
 }
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "docdb-cluster-demo-${count.index}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = "db.t3.medium"
+}
